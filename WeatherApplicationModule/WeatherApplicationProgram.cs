b1c4bfc818d13334603957c4c8432ae383916f3e
Константин <eckumoc@gmail.com>
2022-07-19 17:09:12 +0300

    continue refactoring

---
+++ WeatherApplicationModule/WeatherApplicationProgram.cs
@@ -0,0 +1,10 @@
+﻿\using System;
+
+class WeatherApplicationProgram
+{
+        static void Main(string[] args)
+        {
+            foreach(string arg in args)            
+            	System.Console.WriteLine(arg);
+        }
+}
